6c9d1e82f3e405ddcd3b6d33b46b48b4477bd148
Suriyaraj B <jagandrew13299@gmail.com>
2024-01-20 13:15:03 +0530

    dynamic changes

--- server.js
+++ server.js
@@ -109,6 +109,9 @@ app.put("/image", (req, res) => {
       res.json({ entries: data[0] });
     });
 });
-app.listen(3000, () => {
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("app is running on port 3000");
 });
